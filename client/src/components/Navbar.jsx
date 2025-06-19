bb6368eba2144b81f8c083a6ef3547b5a4d9f5d1
Rishikesh Mahale <rishikeshmahale16@gmail.com>
2025-06-19 21:29:07 +0530

    new files added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,41 @@
+import { Link } from "react-router-dom";
+import Home from "../pages/Home";
+import HeroSection from "./HeroSection";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <nav className="flex flex-col sm:flex-row justify-between items-start sm:items-center px-5 py-5 fixed left-0 top-0 right-0 shadow-md gap-1 sm:gap-0 z-30 bg-white">
+      {/* Logo and site name */}
 
-export default Navbar
+      <div className="flex justify-between items-center">
+        <Link to={"/login"} className="font-bold text-3xl">
+          PicPrism
+        </Link>
+      </div>
+
+      {/* list of other tabs */}
+
+      <ul className="flex gap-5 text-lg font-semibold text-gray-400">
+        <Link to={"/about"} className="hover:text-black cursor-pointer sm:p-2">
+          About
+        </Link>
+        <Link
+          to={"/contact"}
+          className="hover:text-black cursor-pointer sm:p-2"
+        >
+          Contact
+        </Link>
+        <Link to={"/login"} className="hover:text-black cursor-pointer sm:p-2">
+          Log In
+        </Link>
+        <Link to={"/signup"} className="hover:text-black cursor-pointer sm:p-2">
+          Sign up
+        </Link>
+      </ul>
+
+      {/* <HeroSection /> */}
+    </nav>
+  );
+};
+
+export default Navbar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import HeroSection from "../components/HeroSection.jsx"
+
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="mt-36">
+      <HeroSection />
     </div>
   )
 }

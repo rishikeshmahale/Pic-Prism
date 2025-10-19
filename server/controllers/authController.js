4d5cfcbe9178ac42f97823b56793a72a07a725a0
Rishikesh Mahale <rishikeshmahale16@gmail.com>
2025-10-19 22:43:41 +0530

    implemented authentiction and authorization

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require("../models/User.js");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const { generateAccessToken } = require("../helpers/accessToken.js");
+const { generateRefreshToken } = require("../helpers/refreshToken.js");
 
 const signup = async (req, res) => {
   const { username, email, password, accountType } = req.body;
@@ -19,23 +21,73 @@ const signup = async (req, res) => {
     user = new User({
       username,
       email,
-      password : securePassword,
+      password: securePassword,
       accountType,
     });
 
-      await user.save();
+    await user.save();
 
-      return res.status(201).json({ success: true, message : "User created successfuly", user: user });
-      
+    return res
+      .status(201)
+      .json({ success: true, message: "User created successfuly", user: user });
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
 };
 
 const login = async (req, res) => {
+  const { email, password } = req.body;
 
-    return res.json({Message : "Login"})
+  try {
+    let user = await User.findOne({ email });
 
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+
+    let comparePassword = await bcrypt.compare(password, user.password);
+
+    if (!comparePassword) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Password" });
+    }
+
+    const data = {
+      id : user._id,
+      accountType: user.accountType,
+      author : user.username
+    }
+
+    const accessToken = generateAccessToken(data);
+    const refreshToken = generateRefreshToken(data);
+
+    // console.log({
+    //   success: true,
+    //   message: "Login Successful",
+    //   accessToken,
+    //   refreshToken,
+    //   role: user.accountType,
+    //   author: user.username 
+    // })
+
+    return res.status(200).json({
+      success: true,
+      message: "Login Successful",
+      accessToken,
+      refreshToken,
+      role: user.accountType,
+      author: user.username
+    });
+
+
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+
+  return res.json({ Message: "Login" });
 };
 
 module.exports = {
